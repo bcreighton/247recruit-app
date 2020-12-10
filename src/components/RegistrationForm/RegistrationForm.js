ddb97a598211acc89ab618858198f42164a06d46
Bryson Creighton <brysoncreighton@gmail.com>
2020-12-10 02:49:38 -0700

    Initial color scheme implementation

--- src/components/RegistrationForm/RegistrationForm.js
+++ src/components/RegistrationForm/RegistrationForm.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import './RegistrationForm.css'
 
 class RegistrationForm extends Component {
   render() {
     return (
       <section className='container'>
         <form action="" id="registration">
-          <label htmlFor="name">Name:</label>
           <input type="text" id='name' name='user_name' />
+          <label htmlFor="name">Name</label>
 
-          <label htmlFor="email">Email:</label>
           <input type="email" id='email' name='user_email' />
+          <label htmlFor="email">Email</label>
 
-          <label htmlFor="password">Password:</label>
           <input type="password" id='password' name='user_password' />
+          <label htmlFor="password">Password</label>
 
           <button type='submit'><Link to='/search'>Get Started!!!</Link></button>
         </form>
