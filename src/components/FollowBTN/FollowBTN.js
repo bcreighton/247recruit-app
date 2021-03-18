dfc7278952a62fbc032b25d00ca99f267d82c5a2
Bryson Creighton <brysoncreighton@gmail.com>
2021-03-18 01:51:20 -0600

    Update FollowBTN.js
    Developing the follow agent toggle

--- src/components/FollowBTN/FollowBTN.js
+++ src/components/FollowBTN/FollowBTN.js
@@ -5,17 +5,29 @@ import './FollowBTN.css'
 class FollowBTN extends Component {
   static contextType = RecruitContext;
 
-  isAgentFollowed = () => {
-    
+  state = {
+    followed: false,
+  }
+
+  followToggle() {
+    const followed = this.context.followedAgents.find(agent => agent.id === this.props.agentId)
+    debugger;
+
+    (!followed)
+      ? this.setState({ followed: false })
+      : this.setState({ followed: true })
   }
 
   handleClick = () => {
-    this.context.followAgent(this.context.user.id, this.props.agentId)
+    this.context.followAgent(this.context.user.id, this.props.agentId);
+    this.followToggle();
   }
 
   render() {
+    const followStatus = (!this.state.followed) ? `Follow` : `Unfollow`;
+
     return (
-      <button className="follow" onClick={this.handleClick}>Follow</button>
+      <button className="follow" onClick={this.handleClick}>{followStatus}</button>
     )
   }
 }
