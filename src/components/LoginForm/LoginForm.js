48b6f30e0d0e92339faa16308209464aeac8c10c
Bryson Creighton <brysoncreighton@gmail.com>
2021-03-24 17:59:56 -0600

    Add basic auth
    Added Token Service to establish browser level authorization.
    WIP

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import TokenService from '../../services/token-service';
 import './LoginForm.css'
 
 class LoginForm extends Component {
+
+  handleSubmitBasicAuth = ev => {
+    ev.preventDefault();
+    const { loginForm_username, loginForm_password } = ev.target
+
+    TokenService.saveAuthToken(
+      TokenService.makeBasicAuthToken(loginForm_username.value, loginForm_password.value)
+    )
+  }
+
   render() {
     return (
       <section className='container'>
-        <form action="" id="login">
+        <form 
+          id="login"
+          onSubmit={this.handleSubmitBasicAuth}
+        >
+          <input required type="username" id='loginForm_username' name='username' />
+          <label htmlFor="loginForm_username">Username</label>
 
-          <input type="email" id='email' name='user_email' />
-          <label htmlFor="email">Email</label>
+          <input required type="password" id='loginForm_password' name='user_password' />
+          <label htmlFor="loginForm_password">Password</label>
 
-          <input type="password" id='password' name='user_password' />
-          <label htmlFor="password">Password</label>
-
-          <button type='submit'><Link to='/agents'>Sign In</Link></button>
+          <button type='submit'>Sign In</button>
         </form>
       </section>
     )
--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,20 +1,53 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import Brand from '../Brand/Brand'
+import TokenService from '../../services/token-service';
 import './Nav.css'
 
 class Nav extends Component {
-  render() {
+
+  handleLogoutClick = () => {
+    TokenService.clearAuthToken();
+  }
+
+  renderSigninLink() {
     return (
-      <nav>
+      <nav className='nav_not-logged-in'>
         <Brand />
         <ul>
           <li className="navItem"><Link to='/signin'>Sign In</Link></li>
         </ul>
+      </nav>
+    )
+  }
+  
+  renderLogoutLink() {
+    return (
+      <nav className='nav_logged-in'>
+        <Brand />
         <ul>
-          <li className="navItem"><Link to='/agents'>Followed Agents</Link></li>
+          <li className="navItem">
+            <Link to='/agents'>Followed Agents</Link>
+          </li>
+          <li className="navItem">
+            <Link
+              onClick={this.handleLogoutClick}
+              to='/'>
+              Logout
+            </Link>
+          </li>
         </ul>
       </nav>
+    )
+  }
+  
+  render() {
+    return (
+      <>
+        {TokenService.hasAuthToken()
+          ? this.renderLogoutLink()
+          : this.renderSigninLink()}
+      </>
     )
   }
 }
