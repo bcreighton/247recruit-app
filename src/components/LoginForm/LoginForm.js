1eb1fefd7fe2d114c0ab83cb754c1eb707deff69
Bryson Creighton <brysoncreighton@gmail.com>
2021-04-05 18:16:14 -0600

    Implement JWT Login Auth

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import TokenService from '../../services/token-service';
+import AuthApiService from '../../services/auth-api-service';
 import './LoginForm.css'
 
 class LoginForm extends Component {
+
+  state = {
+    error: null,
+  }
 
   handleSubmitBasicAuth = ev => {
     
@@ -15,12 +19,33 @@ class LoginForm extends Component {
     )
   }
 
+  handleSubmitJwtAuth = ev => {
+    ev.preventDefault()
+    this.setState({ error: null })
+
+    const { loginForm_username, loginForm_password } = ev.target;
+
+    AuthApiService.postLogin({
+      username: loginForm_username.value,
+      password: loginForm_password.value,
+    })
+      .then(res => {
+        loginForm_username.value = ''
+        loginForm_password.value = ''
+        TokenService.saveAuthToken(res.authToken)
+        this.props.handleLoginSuccess()
+      })
+      .catch(res => {
+        this.setState({ error: res.error })
+      })
+  }
+
   render() {
     return (
       <section className='container'>
         <form 
           id="login"
-          onSubmit={this.handleSubmitBasicAuth}
+          onSubmit={this.handleSubmitJwtAuth}
         >
           <input required type="username" id='loginForm_username' name='username' />
           <label htmlFor="loginForm_username">Username</label>
--- src/components/Pages/Login/Login.js
+++ src/components/Pages/Login/Login.js
@@ -2,9 +2,22 @@ import React, { Component } from 'react'
 import LoginForm from '../../LoginForm/LoginForm'
 
 class Login extends Component {
+    static defaultProps = {
+        location: {},
+        history: {
+            push: () => {},
+        }
+    }
+
+    handleLoginSuccess = () => {
+        const { location, history } = this.props
+        const destination = (location.state || {}).from || '/search';
+        history.push(destination)
+    }
+    
     render() {
         return (
-            <LoginForm />
+            <LoginForm handleLoginSuccess={this.handleLoginSuccess}/>
         )
     }
 }
