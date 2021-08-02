518a7f8109bfb9dcdba2495c62a524f52c94393e
Bryson Creighton <brysoncreighton@gmail.com>
2021-08-02 11:23:21 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    API_ENDPOINT:  `https://pacific-brushlands-70063.herokuapp.com/api`,
+    API_ENDPOINT: `https://pacific-brushlands-70063.herokuapp.com/api`,
     API_KEY: process.env.REACT_APP_API_KEY
 };
